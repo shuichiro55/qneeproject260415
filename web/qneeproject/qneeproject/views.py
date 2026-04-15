f65bbf5c002641885412d0160c6d843542b37c10
root <root@THIRDWAVE2023.localdomain>
2026-04-15 21:29:03 +0900

    20260415 20:40 小原さんと共有時点2

--- web/qneeproject/qneeproject/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def top_corporate(request):
-  return render(request, 'top_corporate.html')
--- web/qneeproject/qpay/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class QpayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'qpay'
