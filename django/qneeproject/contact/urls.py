83ec8b270286819fa926828c84d5330d41f47be1
root <root@THIRDWAVE2023.localdomain>
2026-04-16 14:55:53 +0900

    20260416 18:10 Invitatio機能完成・履歴検索機能追加前

--- django/qneeproject/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
-from contact import views
-
-urlpatterns = [
-    # Contact
-    path('contactus',views.Contactus.as_view(),name="contactus")
-]
--- django/qneeproject/qneeproject/__init__.py
+++
@@ -1,8 +0,0 @@
-# この下の3行は後で消す 25/02/28
-import sys
-print(f'pass0000')
-print(sys.executable)
-
-import pymysql
-
-pymysql.install_as_MySQLdb()
